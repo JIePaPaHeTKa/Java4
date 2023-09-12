29c1792bfbb763c4b1b3442c18d707e91f70d834
Igor Reus <igorsreus@gmail.com>
2023-09-12 16:05:01 +0300

    second

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,8 +2,7 @@ public class BonusMilesService {
     public int calculate(int price) {
         int rub = 20;       // количество рублей в соотношении рублей к миле
         int miles = 1;      // количество миль в соотношении рублей к миле
-        int percent = miles * 100 / rub;    // процент начислениия расчитывается в соотношении рублей к миле
-        int bonus = price * percent / 100;  // формула подсчета количества бонусов
+        int bonus = price * miles / rub;  // формула подсчета количества бонусов
         System.out.println("Итоговый бонус составит: "); //итог
         return bonus;
     }
